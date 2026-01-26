e3f2a1aac2907b6e813175112f6c092c03758ff8
Иван Лагунов <mak.ivan89@gmail.com>
2026-01-27 01:33:16 +0300

    Import cache token fix 3

--- arknights-tracker/src/lib/components/Icons.svelte
+++ arknights-tracker/src/lib/components/Icons.svelte
@@ -2084,8 +2084,8 @@
     </svg>
 {:else if name == "save"}
     <svg
-        width="48"
-        height="48"
+        width="18"
+        height="18"
         viewBox="0 0 48 48"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
