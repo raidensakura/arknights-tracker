74c870d911856bd01fb7afb99ce04ff370f8bdf3
Иван Лагунов <mak.ivan89@gmail.com>
2026-02-05 16:09:42 +0300

    Icon fixes n.999

--- arknights-tracker/src/routes/+layout.svelte
+++ arknights-tracker/src/routes/+layout.svelte
@@ -49,12 +49,15 @@
 
   <link rel="manifest" href="/site.webmanifest" />
 
-  <link rel="icon" href="/favicon.ico" sizes="any" />
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
+  <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <meta name="apple-mobile-web-app-title" content="Goyfield.moe" />
 
   <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
   <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
