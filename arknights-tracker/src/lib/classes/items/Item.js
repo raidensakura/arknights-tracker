39dc048b0c7090ab652a1e59ee72512aded50491
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-05-20 21:18:02 +0500

    item classes

--- arknights-tracker/src/lib/classes/items/Item.js
+++ arknights-tracker/src/lib/classes/items/Item.js
@@ -1,4 +1,6 @@
 import {items} from "$lib/data/items/items.js";
+import {Fuel} from "$lib/classes/items/Fuel.js";
+import {FullBottle} from "$lib/classes/items/FullBottle.js";
 
 export class Item {
     _itemObj;
@@ -17,6 +19,22 @@ export class Item {
 
     get groupId() {
         return this._itemObj.groupId;
+    }
+
+    isFuel() {
+        return Fuel.isFuel(this.id);
+    }
+
+    toFuel() {
+        return Fuel.getFuelFromItem(this);
+    }
+
+    isFullBottle() {
+        return FullBottle.isFullBottle(this.id);
+    }
+
+    toFullBottle() {
+        return FullBottle.getFullBottleFromItem(this);
     }
 
     static getItem(itemId) {
