3c8d3efa575216282e618d1371c8f08d2814f6a6
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-05-23 20:05:12 +0500

    building icon ids

--- arknights-tracker/src/lib/data/buildings/buildings.js
+++ arknights-tracker/src/lib/data/buildings/buildings.js
@@ -2,116 +2,139 @@ export const buildings = {
     "component_mc_1": {
         "id": "component_mc_1",
         "type": "crafter",
-        "itemId": "item_port_cmpt_mc_1"
+        "itemId": "item_port_cmpt_mc_1",
+        "iconId": "icon_port_cmpt_mc_1"
     },
     "dismantler_1": {
         "id": "dismantler_1",
         "type": "crafter",
-        "itemId": "item_port_dismantler_1"
+        "itemId": "item_port_dismantler_1",
+        "iconId": "icon_port_dismantler_1"
     },
     "liquid_purifier_1": {
         "id": "liquid_purifier_1",
         "type": "crafter",
-        "itemId": "item_port_liquid_purifier_1"
+        "itemId": "item_port_liquid_purifier_1",
+        "iconId": "icon_port_liquid_purifier_1"
     },
     "filling_powder_mc_1": {
         "id": "filling_powder_mc_1",
         "type": "crafter",
-        "itemId": "item_port_filling_pd_mc_1"
+        "itemId": "item_port_filling_pd_mc_1",
+        "iconId": "icon_port_filling_pd_mc_1"
     },
     "furnance_1": {
         "id": "furnance_1",
         "type": "crafter",
-        "itemId": "item_port_furnance_1"
+        "itemId": "item_port_furnance_1",
+        "iconId": "icon_port_furnance_1"
     },
     "furnance_nop_1": {
         "id": "furnance_nop_1",
         "type": "crafter",
-        "itemId": "item_port_furnance_nop_1"
+        "itemId": "item_port_furnance_nop_1",
+        "iconId": "icon_port_furnance_1"
     },
     "grinder_1": {
         "id": "grinder_1",
         "type": "crafter",
-        "itemId": "item_port_grinder_1"
+        "itemId": "item_port_grinder_1",
+        "iconId": "icon_port_grinder_1"
     },
     "seedcollector_1": {
         "id": "seedcollector_1",
         "type": "crafter",
-        "itemId": "item_port_seedcol_1"
+        "itemId": "item_port_seedcol_1",
+        "iconId": "icon_port_seedcol_1"
     },
     "mix_pool_1": {
         "id": "mix_pool_1",
         "type": "crafter",
-        "itemId": "item_port_mix_pool_1"
+        "itemId": "item_port_mix_pool_1",
+        "iconId": "icon_port_mix_pool_1"
     },
     "mix_pool_2": {
         "id": "mix_pool_2",
         "type": "crafter",
-        "itemId": "item_port_mix_pool_2"
+        "itemId": "item_port_mix_pool_2",
+        "iconId": "icon_port_mix_pool_2"
     },
     "planter_1": {
         "id": "planter_1",
         "type": "crafter",
-        "itemId": "item_port_planter_1"
+        "itemId": "item_port_planter_1",
+        "iconId": "icon_port_planter_1"
     },
     "shaper_1": {
         "id": "shaper_1",
         "type": "crafter",
-        "itemId": "item_port_shaper_1"
+        "itemId": "item_port_shaper_1",
+        "iconId": "icon_port_shaper_1"
     },
     "winder_1": {
         "id": "winder_1",
         "type": "crafter",
-        "itemId": "item_port_winder_1"
+        "itemId": "item_port_winder_1",
+        "iconId": "icon_port_winder_1"
     },
     "thickener_1": {
         "id": "thickener_1",
         "type": "crafter",
-        "itemId": "item_port_thickener_1"
+        "itemId": "item_port_thickener_1",
+        "iconId": "icon_port_thickener_1"
     },
     "tools_assebling_mc_1": {
         "id": "tools_assebling_mc_1",
         "type": "crafter",
-        "itemId": "item_port_tools_asm_mc_1"
+        "itemId": "item_port_tools_asm_mc_1",
+        "iconId": "icon_port_tools_asm_mc_1"
     },
     "xiranite_oven_1": {
         "id": "xiranite_oven_1",
         "type": "crafter",
-        "itemId": "item_port_xiranite_oven_1"
+        "itemId": "item_port_xiranite_oven_1",
+        "iconId": "icon_port_xiranite_oven_1"
     },
     "power_station_1": {
         "id": "power_station_1",
         "type": "powerStation",
-        "itemId": "item_port_power_sta_1"
+        "itemId": "item_port_power_sta_1",
+        "iconId": "icon_port_power_sta_1"
     },
     "miner_1": {
         "id": "miner_1",
         "type": "miner",
-        "itemId": "item_port_miner_1"
+        "itemId": "item_port_miner_1",
+        "iconId": "icon_port_miner_1"
     },
     "miner_2": {
         "id": "miner_2",
         "type": "miner",
-        "itemId": "item_port_miner_2"
+        "itemId": "item_port_miner_2",
+        "iconId": "icon_port_miner_2"
     },
     "miner_3": {
         "id": "miner_3",
         "type": "miner",
-        "itemId": "item_port_miner_3"
+        "itemId": "item_port_miner_3",
+        "iconId": "icon_port_miner_3"
     },
     "miner_4": {
         "id": "miner_4",
         "type": "miner",
-        "itemId": "item_port_miner_4"
+        "itemId": "item_port_miner_4",
+        "iconId": "icon_port_liquid_miner"
     },
     "pump_1": {
         "id": "pump_1",
         "type": "pump",
-        "itemId": "item_port_pump_1"
+        "itemId": "item_port_pump_1",
+        "iconId": "icon_port_pump_1"
     },
     "pump_2": {
         "id": "pump_2",
         "type": "pump",
-        "itemId": "item_port_pump_2"
+        "itemId": "item_port_pump_2",
+        "iconId": "icon_port_pump_2"
     }
 }
