394cffffd49408f153e44a948121aae6e5be6759
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-06-16 17:17:22 +0500

    using dropdown template

--- arknights-tracker/src/lib/components/dataToolbarV2/filterDropdowns/RecipesFilterDropdown.svelte
+++ arknights-tracker/src/lib/components/dataToolbarV2/filterDropdowns/RecipesFilterDropdown.svelte
@@ -1,5 +1,6 @@
 <script>
     import { FactoryEvent } from "$lib/classes/events/FactoryEvent.js";
+    import DropdownTemplate from "$lib/components/dataToolbarV2/DropdownTemplate.svelte";
     import Icon from "$lib/components/Icon.svelte";
     import { t } from "$lib/i18n";
 
@@ -68,25 +69,10 @@
     };
 </script>
 
-<div
-    class="z-[100] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[calc(100vw-2rem)] max-w-[350px] max-h-[85vh] sm:relative sm:w-[500px] sm:max-w-[calc(100vw-2rem)] sm:transform-none sm:left-0 dark:bg-[#383838] dark:border-[#444444] bg-[#F2F2F2] rounded-2xl shadow-2xl border border-gray-200 p-5 flex flex-col gap-3 outline-none overflow-y-auto"
-    role="dialog"
-    aria-modal="true"
-    tabindex="-1"
-    on:click|stopPropagation
-    on:keydown|stopPropagation
+<DropdownTemplate
+    showResetButton={true}
+    onResetButton={onFilterReset}
 >
-
-    <div class="flex justify-start">
-
-        <button
-            class="text-xs font-bold text-gray-500 mb-2 dark:text-gray-400 border border-gray-300 dark:border-[#444444] rounded-full px-4 py-1.5 hover:text-[#F9B90C] hover:border-[#F9B90C] hover:bg-white hover:dark:bg-[#FFB200]/80 hover:dark:border-[#FFB200] hover:dark:text-[#E0E0E0] transition-all uppercase tracking-wider"
-            on:click={onFilterReset}
-        >
-            {$t("sort.reset")}
-        </button>
-
-    </div>
 
     <div>
 
@@ -243,4 +229,4 @@
 
     </div>
 
-</div>
+</DropdownTemplate>
