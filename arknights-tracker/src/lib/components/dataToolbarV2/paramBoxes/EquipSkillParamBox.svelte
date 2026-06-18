3752f7ee096b6411123c99077ed7fae1b727eb01
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-06-18 17:14:41 +0500

    add equip param boxes

---
+++ arknights-tracker/src/lib/components/dataToolbarV2/paramBoxes/EquipSkillParamBox.svelte
@@ -0,0 +1,32 @@
+<script>
+    import TextParamBoxTemplate from "$lib/components/dataToolbarV2/paramBoxes/TextParamBoxTemplate.svelte";
+    import Icon from "$lib/components/Icon.svelte";
+    import { t } from "$lib/i18n";
+
+    export let styleMode;
+
+    export let paramId;
+    export let getLocaleFunc = (paramId) => $t(`equipmentTypes.${paramId}`);
+
+    $: statIconId = paramId.toLowerCase() === "maxhp"
+        ? "hp"
+        : paramId.toLowerCase()
+</script>
+
+<TextParamBoxTemplate styleMode={styleMode}>
+
+    <div
+        slot="left"
+        class="w-5 h-5 bg-[#2A2A2A] rounded-[4px] flex items-center justify-center pointer-events-none"
+    >
+
+        <Icon
+            name="{statIconId}"
+            class="w-3 h-3 text-white pointer-events-none"
+        />
+
+    </div>
+
+    {getLocaleFunc(paramId)}
+
+</TextParamBoxTemplate>
